cf05e3663610303db1837cdae3871a81fbeb7f16
Renan Augusto Pupin de Oliveira <renan.pupin@gmail.com>
2016-05-03 23:05:47 -0300

    app start

---
+++ resources/views/index.blade.php
@@ -0,0 +1,121 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+    <title>VegWrap</title>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <!--[if lte IE 8]><script src="{!! asset('assets/js/ie/html5shiv.js') !!}"></script><![endif]-->
+    <link rel="stylesheet" href="{!! asset('assets/css/main.css') !!}" />
+    <!--[if lte IE 8]><link rel="stylesheet" href="{!! asset('assets/css/ie8.css') !!}" /><![endif]-->
+    <!--[if lte IE 9]><link rel="stylesheet" href="{!! asset('assets/css/ie9.css') !!}" /><![endif]-->
+</head>
+<body>
+
+<!-- Header -->
+<section id="header">
+    <header class="major">
+        <h1>VEG WRAP</h1>
+        <p>Porque ser { vegetariano & Saudável } é mais legal</p>
+    </header>
+    <div class="container">
+        <ul class="actions">
+            <li><a href="#one" class="button special scrolly">Fazer Pedido</a></li>
+            <li><a href="#one" class="button special scrolly">Conhecer</a></li>
+        </ul>
+    </div>
+
+</section>
+
+<!-- One -->
+<section id="one" class="main special">
+    <div class="container">
+        <span class="image fit primary"><img src="assets/images/8.jpg" alt="" /></span>
+        <div class="content">
+            <header class="major">
+                <h2>Quem somos?</h2>
+            </header>
+            <p>Somos uma empresa que busca levar ao cliente uma experiência diferente, onde ele saiba que se está comendo algo delicioso, e ao mesmo tempo trazer empatia, com um estilo de vida saudável e harmônico com o meio ambiente e contra o sofrimento dos animais.</p>
+        </div>
+        <a href="#two" class="goto-next scrolly">Next</a>
+    </div>
+</section>
+
+<!-- Two -->
+<section id="two" class="main special">
+    <div class="container">
+        <span class="image fit primary"><img src="assets/images/9.jpg" alt="" /></span>
+        <div class="content">
+            <header class="major">
+                <h2>Coisas que oferecemos</h2>
+            </header>
+            <p>Abaixo estão algumas das coisas que desejamos passar aos nossos clientes</p>
+            <ul class="icons-grid">
+                <li>
+                    <span class="icon major fa-check-square-o"></span>
+                    <h3>Alimento delicioso</h3>
+                </li>
+                <li>
+                    <span class="icon major fa-usd"></span>
+                    <h3>Preço baixo</h3>
+                </li>
+                <li>
+                    <span class="icon major fa-paw"></span>
+                    <h3>Conscientização Animal</h3>
+                </li>
+                <li>
+                    <span class="icon major fa-heartbeat"></span>
+                    <h3>Estilo de vida <br>saudável</h3>
+                </li>
+            </ul>
+        </div>
+        <a href="#three" class="goto-next scrolly">Next</a>
+    </div>
+</section>
+
+<!-- Three -->
+<section id="three" class="main special">
+    <div class="container">
+        <span class="image fit primary"><img src="assets/images/13.jpg" alt="" /></span>
+        <div class="content">
+            <header class="major">
+                <h2>Mais uma coisa</h2>
+            </header>
+            <p>Vegetarianismo não é apenas deixar de comer carne – seja ela vermelha, de ave, de peixe ou produtos derivados – mas adotar hábitos alimentares mais saudáveis e nutritivos. A dieta é baseada no consumo de cereais integrais, hortaliças, frutas, leguminosas, grãos e sementes -
+                sem contar com as motivações ligadas aos direitos dos animais e às preocupações ambientais.</p>
+        </div>
+        <a href="#footer" class="goto-next scrolly">Next</a>
+    </div>
+</section>
+
+<!-- Footer -->
+<section id="footer">
+    <div class="container">
+        <header class="major">
+            <h2>Você ainda tem dúvida? <br> Agora você está pronto :)</h2>
+        </header>
+        <ul class="actions">
+            <li><input type="button" value="Fazer Pedido" class="special" /></li>
+        </ul>
+    </div>
+    <footer>
+        <ul class="icons">
+            <li><a href="#" class="icon alt fa-facebook"><span class="label">Facebook</span></a></li>
+            <li><a href="#" class="icon alt fa-envelope"><span class="label">Email</span></a></li>
+        </ul>
+        <ul class="copyright">
+            <li>&copy; VegWrap 2016</li><li>Todos os direitos reservados</li>
+        </ul>
+    </footer>
+</section>
+
+<!-- Scripts -->
+<script src="{!! asset('assets/js/jquery.min.js') !!}"></script>
+<script src="{!! asset('assets/js/jquery.scrollex.min.js') !!}"></script>
+<script src="{!! asset('assets/js/jquery.scrolly.min.js') !!}"></script>
+<script src="{!! asset('assets/js/skel.min.js') !!}"></script>
+<script src="{!! asset('assets/js/util.js') !!}"></script>
+<!--[if lte IE 8]><script src="{!! asset('assets/js/ie/respond.min.js') !!}"></script><![endif]-->
+<script src="{!! asset('assets/js/main.js') !!}"></script>
+
+</body>
+</html>
